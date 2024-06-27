f41394cd4132a854f66586ce8fbbdc341a35a1d9
Weslleyqueiroz <weslleyqueiroz773@gmail.com>
2024-06-27 19:44:20 -0300

    Tipos- Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
+
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
+        String meuNome;
+        meuNome = "weslley";
+        System.out.println(meuNome);
     }
 }
